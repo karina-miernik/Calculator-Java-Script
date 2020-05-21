555fa71a44fade99846125da358c435e64f47c5d
Karina Miernik <karina.miernik@gmail.com>
2020-05-21 19:03:03 +0200

    listener for inputs

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-// grabbing each button - DOM
 
-const num1 = document.querySelector(".--vaule-value-1")
-const num2 = document.querySelector(".--vaule-value-2")
-const num3 = document.querySelector(".--vaule-value-3")
-const num4 = document.querySelector(".--vaule-value-4")
-const num5 = document.querySelector(".--vaule-value-5")
-const num6 = document.querySelector(".--vaule-value-6")
-const num7 = document.querySelector(".--vaule-value-7")
-const num8 = document.querySelector(".--vaule-value-8")
-const num9 = document.querySelector(".--vaule-value-9")
-const num0 = document.querySelector(".--vaule-value-0")
-const canc = document.querySelector(".--vaule-canc")
-const plusmin = document.querySelector(".--vaule-plusmin")
-const del = document.querySelector(".--vaule-del")
-const perc = document.querySelector(".--vaule-perc")
-const divide = document.querySelector(".--vaule-divide")
-const minus = document.querySelector(".--vaule-minus")
-const dot = document.querySelector(".--vaule-dot")
-const plus = document.querySelector(".--vaule-plus")
-
-
-
-const equal = document.querySelector(".--vaule-equal")
-
-
-equal.addEventListener('click', (event) => {
-    return console.log('result')
+const calculator = document.querySelector('.calc')
+const calcInput = calculator.querySelector('.calc__input')
+calcInput.addEventListener('click', (e) => {
+    alert('hello there!')
 })
