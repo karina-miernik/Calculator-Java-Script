5f848cc19b6e7993e55b08619b94ea2baadb97b5
Karina Miernik <karina.miernik@gmail.com>
2020-05-28 09:56:36 +0200

    new js file and DOM elements

---
+++ calc.js
@@ -0,0 +1,9 @@
+
+// DOM elements
+const numberBtn = document.querySelectorAll('[data-number]')
+const operationBtn = document.querySelectorAll('[data-operation]')
+const equalBtn = document.querySelector('[data-equal]')
+const deleteBtn = document.querySelector('[data-delete]')
+const clearBtn = document.querySelector('[data-clear]')
+const previousOutput = document.querySelector('[data-previous]')
+const currentOutput = document.querySelector('[data-current]')
