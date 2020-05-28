d3c65760137389ea724fa58a055edae214bc39ec
Karina Miernik <karina.miernik@gmail.com>
2020-05-28 10:32:17 +0200

    added class with constructor for calculator

--- calc.js
+++ calc.js
@@ -1,3 +1,19 @@
+// creating Calculator class with methods
+
+class Calculator {
+    constructor(previousOutput, currentOutput) {
+        this.previousOutput = previousOutput;
+        this.currentOutput = currentOutput;
+        this.clear()
+    }
+
+    clear() {
+        this.previousOutput = '';
+        this.currentOutput = ''
+        this.operation = ''
+    }
+}
+
 
 
 // DOM elements
@@ -8,3 +24,5 @@ const deleteBtn = document.querySelector('[data-delete]')
 const clearBtn = document.querySelector('[data-clear]')
 const previousOutput = document.querySelector('[data-previous]')
 const currentOutput = document.querySelector('[data-current]')
+
+const calculator = new Calculator(previousOutput, currentOutput)
